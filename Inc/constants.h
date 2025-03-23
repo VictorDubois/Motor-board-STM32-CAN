229e06b0f140a53a97f5b4d241f5076fcbf058b4
Victor Dubois <victor.dubois@navya.tech>
2025-03-23 16:48:23 +0100

    tune for KV2

--- Inc/constants.h
+++ Inc/constants.h
@@ -11,9 +11,13 @@
 
 #define TICKS_PER_REVOLUTION 8192.0 //360    // Nb ticks per wheel revolution
 #define WHEEL_RADIUS 19.91               // Diameter of the wheel (mm) UNUSED !!!
-#define DIST_PER_REVOLUTION WHEEL_RADIUS*M_PI // 217.879 // 214.66 // 206.5//214.635// 210.481 //304.734 // Distance traveled for a full 	wheel revolution (in mm)
+#define DIST_PER_REVOLUTION_THEORY WHEEL_RADIUS*M_PI // 217.879 // 214.66 // 206.5//214.635// 210.481 //304.734 // Distance traveled for a full 	wheel revolution (in mm)
+#define DIST_PER_REVOLUTION DIST_PER_REVOLUTION_THEORY/1.2471814155578613 // 217.879 // 214.66 // 206.5//214.635// 210.481 //304.734 // Distance traveled for a full 	wheel revolution (in mm)
 #define VOIE 205.10
-#define TICKS_PER_DEG VOIE * TICKS_PER_REVOLUTION/(DIST_PER_REVOLUTION*360.0) //450.0//91.472 // 91.290 // 91.108345 // 91.001144 // 91.055 	// 89.28//89//89.06 //4.86          // Nb of diff tick (enc1 - enc2) it takes to rotate 1 deg (old value: 4.7)
+#define TICKS_PER_DEG VOIE * 0.96007158213* TICKS_PER_REVOLUTION/(DIST_PER_REVOLUTION_THEORY*360.0) //450.0//91.472 // 91.290 // 91.108345 // 91.001144 // 91.055 	// 89.28//89//89.06 //4.86          // Nb of diff tick (enc1 - enc2) it takes to rotate 1 deg (old value: 4.7)
 #define TICKS_OVERFLOW 65536.0
 #define TICKS_half_OVERFLOW TICKS_OVERFLOW/2.0
 #endif
+
+//0.96224690902
+//(1.03775309098)
