d479c16b061f8efa6cfe2ff643ae5877d5f8bed2
Victor Dubois <victor.dubois@navya.tech>
2025-03-25 13:37:41 +0100

    tune for KV2
    # Conflicts:
    #	Inc/constants.h

--- Inc/constants.h
+++ Inc/constants.h
@@ -10,18 +10,19 @@
 #define KV2
 
 #ifdef KV1
-#define TICKS_PER_REVOLUTION 4096 //360    // Nb ticks per wheel revolution
-#define WHEEL_DIAM 68               // Diameter of the wheel (mm) UNUSED !!!
-#define DIST_PER_REVOLUTION 216.2695 // 217.879 // 214.66 // 206.5//214.635// 210.481 //304.734 // Distance traveled for a full 	wheel revolution (in mm)
-#define TICKS_PER_DEG 91.472 // 91.290 // 91.108345 // 91.001144 // 91.055 	// 89.28//89//89.06 //4.86          // Nb of diff tick (enc1 - enc2) it takes to rotate 1 deg (old value: 4.7)
+#define TICKS_PER_REVOLUTION 4096 	// Nb ticks per wheel revolution
+#define WHEEL_DIAM 68               	// Diameter of the wheel (mm) UNUSED !!!
+#define DIST_PER_REVOLUTION 216.2695  	// Distance traveled for a full wheel revolution (in mm)
+#define TICKS_PER_DEG 91.472       	// Nb of diff tick (enc1 - enc2) it takes to rotate 1 deg (old value: 4.7)
 #endif
 
 #ifdef KV2
-#define TICKS_PER_REVOLUTION 8192.0 //360    // Nb ticks per wheel revolution
-#define WHEEL_RADIUS 19.91               // Diameter of the wheel (mm) UNUSED !!!
-#define DIST_PER_REVOLUTION WHEEL_RADIUS*M_PI // 217.879 // 214.66 // 206.5//214.635// 210.481 //304.734 // Distance traveled for a full 	wheel revolution (in mm)
+#define TICKS_PER_REVOLUTION 8192.0 	// Nb ticks per wheel revolution
+#define WHEEL_RADIUS 19.91              // Diameter of the wheel (mm) UNUSED !!!
+#define DIST_PER_REVOLUTION_THEORY WHEEL_RADIUS*M_PI // Distance traveled for a fulli wheel revolution (in mm)
+#define DIST_PER_REVOLUTION DIST_PER_REVOLUTION_THEORY/1.2471814155578613 // Distance traveled for a full wheel revolution (in mm)
 #define VOIE 205.10
-#define TICKS_PER_DEG VOIE * TICKS_PER_REVOLUTION/(DIST_PER_REVOLUTION*360.0) //450.0//91.472 // 91.290 // 91.108345 // 91.001144 // 91.055 	// 89.28//89//89.06 //4.86          // Nb of diff tick (enc1 - enc2) it takes to rotate 1 deg (old value: 4.7)
+#define TICKS_PER_DEG VOIE * 0.96007158213* TICKS_PER_REVOLUTION/(DIST_PER_REVOLUTION_THEORY*360.0) // Nb of diff tick (enc1 - enc2) it takes to rotate 1 deg (old value: 4.7)
 #endif
 
 #define TICKS_OVERFLOW 65536.0
